5243dcd0c9871c6ffcb65e1208d6a342a52c87c9
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-09-10 19:56:11 +0300

    feat(domain): add ReservationStatus enum

---
+++ API/Booking/Booking.Domain/Enums/ReservationStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Enums
+{
+    public enum ReservationStatus
+    {
+        Pending = 0,
+        Confirmed = 1,
+        Cancelled = 2,
+        Completed = 3
+    }
+}
