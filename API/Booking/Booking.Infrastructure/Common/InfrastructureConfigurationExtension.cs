de565c2e5662a99463760fdf5bb38140da5e597f
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-09-12 01:37:08 +0300

    feat(config): add jwt, stripe, openAI, and extension configurations

---
+++ API/Booking/Booking.Infrastructure/Common/InfrastructureConfigurationExtension.cs
@@ -0,0 +1,87 @@
+﻿using Booking.Application.Common.Interfaces;
+using Booking.Domain.Entities;
+using Booking.Infrastructure.Persistence;
+using Booking.Infrastructure.Persistence.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Infrastructure.Common
+{
+    public static class InfrastructureConfigurationExtension
+    {
+        public static IServiceCollection ConfigureInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddLogging();
+
+            // Add DbContext
+            services.AddDbContext<BookingDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("CFG")));
+
+            services.AddEndpointsApiExplorer();
+
+            // Add Identity
+            services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 12;
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedAccount = false;
+            }).AddEntityFrameworkStores<BookingDbContext>();
+
+            // Add Authentication with JWT
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = false, // TODO: Turnned in prod environment
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audiance"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SigningKey"]))
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = ctx =>
+                    {
+                        ctx.Request.Cookies.TryGetValue("access_token", out var accessToken);
+
+                        if (!string.IsNullOrEmpty(accessToken))
+                        {
+                            ctx.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+            // Register repositories
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+
+}
+
