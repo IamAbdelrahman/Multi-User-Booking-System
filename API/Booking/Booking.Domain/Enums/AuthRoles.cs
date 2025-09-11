102b73f05ef7ff2f76fe3913f6edd821cff3b79a
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-09-12 01:32:53 +0300

    feat(domain): add auth-roles enum

---
+++ API/Booking/Booking.Domain/Enums/AuthRoles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Enums
+{
+    [Flags]
+    public enum AuthRoles
+    {
+        Admin = 1,
+        Host = 2,
+        Customer = 4,
+    }
+}
