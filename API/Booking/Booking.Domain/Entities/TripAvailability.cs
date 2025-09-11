c0d977e36e9196cd8e11f4e520ab91aba4410ace
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-09-12 01:32:32 +0300

    feat(domain): add trip-availability entity

---
+++ API/Booking/Booking.Domain/Entities/TripAvailability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Entities
+{
+    public class TripAvailability:BaseEntity<int>
+    {
+        public string TripId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public virtual Trip Trip { get; set; }
+    }
+}
