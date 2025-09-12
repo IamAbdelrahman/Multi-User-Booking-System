20b42cd29948d91d91baf84addbf3b12674d3943
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-09-13 01:39:14 +0300

    feat(dto): add helpers dto

---
+++ API/Booking/Booking.Application/Common/PaginatedResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Application.Common
+{
+    public class PaginatedResult<TEntity>
+    {
+        public IEnumerable<TEntity> Items { get; set; }
+        public PaginationMetaData MetaData { get; set; }
+    }
+}
