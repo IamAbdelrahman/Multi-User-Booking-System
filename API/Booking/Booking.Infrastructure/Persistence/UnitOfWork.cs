4f1316dc07fca8d5ad3db20eb9710c2d2c8f7484
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-09-13 19:47:58 +0300

    feat(uow): add new repos in uow

--- API/Booking/Booking.Infrastructure/Persistence/UnitOfWork.cs
+++ API/Booking/Booking.Infrastructure/Persistence/UnitOfWork.cs
@@ -1,4 +1,7 @@
-﻿using Booking.Application.Common.Interfaces;
+﻿using AutoMapper;
+using Booking.Application.Common.Interfaces;
+using Booking.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +12,32 @@ namespace Booking.Infrastructure.Persistence
 {
     public class UnitOfWork:IUnitOfWork
     {
+        private readonly BookingDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public UnitOfWork(BookingDbContext dbContext, IMapper mapper) 
+        { 
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
 
+        private IReservationRepository? _reservationRepository;
+        private ITripRepository? _tripRepository;
+        private ITripAvailabilityRepository? _tripAvailabilityRepository;
+        public IReservationRepository ReservationRepo => _reservationRepository ??= new ReservationRepository(_dbContext, _mapper);
+        public ITripRepository TripRepo => _tripRepository ??= new TripRepository(_dbContext, _mapper);
+        public ITripAvailabilityRepository TripAvailabilitiyRepo => _tripAvailabilityRepository ??= new TripAvailabilityRepository(_dbContext, _mapper);
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+        public async Task<int> CompleteAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
     }
 }
