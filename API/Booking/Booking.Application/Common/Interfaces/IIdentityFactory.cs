94df297a4b697a32617d082be9f2033ae2674e2e
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-09-13 19:50:42 +0300

    feat(factory): add factory for Identity layer

---
+++ API/Booking/Booking.Application/Common/Interfaces/IIdentityFactory.cs
@@ -0,0 +1,17 @@
+﻿using Booking.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Application.Common.Interfaces
+{
+    public interface IIdentityFactory
+    {
+        public UserManager<User> UserManager { get; }
+        public RoleManager<IdentityRole> RoleManager { get; }
+        public SignInManager<User> SignInManager { get; }
+    }
+}
---
+++ API/Booking/Booking.Infrastructure/Common/IdentityFactory.cs
@@ -0,0 +1,34 @@
+﻿using Booking.Application.Common.Interfaces;
+using Booking.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Infrastructure.Common
+{
+    public class IdentityFactory : IIdentityFactory
+    {
+        private readonly IServiceProvider _provider;
+        private UserManager<User> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private SignInManager<User> _signInManager;
+
+        public IdentityFactory(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public UserManager<User> UserManager => _userManager ??= 
+            _provider.GetRequiredService<UserManager<User>>();
+
+        public RoleManager<IdentityRole> RoleManager => _roleManager ??= 
+            _provider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        public SignInManager<User> SignInManager => _signInManager ??=
+            _provider.GetRequiredService<SignInManager<User>>();
+    }
+}
