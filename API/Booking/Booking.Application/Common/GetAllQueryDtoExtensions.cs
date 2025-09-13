71b9e4814774c1ee4dad992fd67cd30d79da6c2c
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-09-13 19:52:59 +0300

    feat(extension): add extension method for pagination's query

---
+++ API/Booking/Booking.Application/Common/GetAllQueryDtoExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Application.Common
+{
+    public static class GetAllQueryDtoExtensions
+    {
+        public static int CalcSkippedItems(this GetAllQueryDto dto)
+        {
+            return (dto.Page - 1) * dto.PageSize;
+        }
+    }
+}
