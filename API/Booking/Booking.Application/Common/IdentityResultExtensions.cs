9c47eae076318ffa67e122956974a7f969e79660
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-09-13 19:51:59 +0300

    feat(extension): add extension method for identity layer

---
+++ API/Booking/Booking.Application/Common/IdentityResultExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Application.Common
+{
+    public static class IdentityResultExtensions
+    {
+        public static List<string> ToErrorList(this IdentityResult result)
+        {
+            return result.Errors.Select(e => $"{e.Code}: {e.Description}").ToList();
+        }
+    }
+}
